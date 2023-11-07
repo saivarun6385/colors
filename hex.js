f36d41a894edb3ab94a4c0a54b50732a506cd6fb
saivarun6385 <84833239+saivarun6385@users.noreply.github.com>
2023-11-07 14:37:11 +0530

    Add files via upload

---
+++ hex.js
@@ -0,0 +1,16 @@
+const colors=[0,1,2,3,4,5,6,7,8,9,'a','b','c','d','e','f']
+const btn=document.getElementById("btn")
+const color=document.querySelector(".color")
+
+
+btn.addEventListener("click",function(){
+    let hecol='#'
+    for(let i=0;i<6;i++){
+        hecol+=colors[getRandomNumber()]
+    }
+    document.body.style.backgroundColor=hecol
+    color.innerText=hecol
+})
+function getRandomNumber(){
+    return Math.floor(Math.random()*colors.length)
+}
